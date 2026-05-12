562217c83801a815dd43f3dfa7dde10d9ce05dc6
ianspy <ianspy@outlook.com>
2026-05-12 14:11:42 +0800

    新增访问人数

--- src/App.tsx
+++ src/App.tsx
@@ -215,14 +215,9 @@ function App() {
           
           {/* 访客数量统计框 */}
           <div className="visitor-card">
-            <div className="visitor-icon">
-              <svg height="20" viewBox="0 0 24 24" width="20" fill="currentColor">
-                <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
-              </svg>
-            </div>
             <div className="visitor-info">
-              <div className="visitor-count">{visitorCount.toLocaleString()}</div>
-              <div className="visitor-label">总访问量</div>
+              <div className="visitor-label">今日访问</div>
+              <div className="visitor-count">{visitorCount} 人</div>
             </div>
           </div>
         </aside>
